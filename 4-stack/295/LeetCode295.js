e65894d456307af40cd96922aef969dfe3c2ffe9
Toryang <toryangchen@outlook.com>
2019-10-10 14:01:00 +0800

    update leetcode 295

--- 4-stack/295/LeetCode295.js
+++ 4-stack/295/LeetCode295.js
@@ -2,12 +2,7 @@
  * initialize your data structure here.
  */
 var MedianFinder = function() {
-	this.maxHeap = [];
-	this.minHeap = [];
-	this.max_maxHeap = Number.MIN_VALUE;
-	this.min_maxHeap = Number.MAX_VALUE;
-	this.max_minHeap = Number.MIN_VALUE;
-	this.min_minHeap = Number.MAX_VALUE;
+	this.arr = [];
 };
 
 /**
@@ -15,69 +10,33 @@ var MedianFinder = function() {
  * @return {void}
  */
 MedianFinder.prototype.addNum = function(num) {
-
-	if (this.maxHeap.length === this.minHeap.length) {
-		this.saveInMaxHeap(num);
-		return;
-	}
-
-	if (this.minHeap.length === 0) {
-		if (this.min_maxHeap < num) {
-			this.max_maxHeap = Number.MIN_VALUE;
-			this.min_maxHeap = Number.MAX_VALUE;
-			var pre = this.maxHeap.pop();
-			this.saveInMaxHeap(num);
-			this.saveInMinHeap(pre.val);
-		} else {
-			this.saveInMinHeap(num);
+	const bs = (n) => {
+		let start = 0;
+		let end = this.arr.length;
+		while (start < end) {
+			let mid = (start + end) >> 1;
+			if (n > this.arr[mid]) {
+				start = mid + 1;
+			} else {
+				end = mid;
+			}
 		}
-		return;
+		this.arr.splice(start, 0, n);
 	}
 
-	if (this.maxHeap[this.maxHeap.length - 1].min > num) {
-		this.saveInMinHeap(num);
+	if (this.arr.length === 0) {
+		this.arr.push(num);
 	} else {
-		this.max_maxHeap = Number.MIN_VALUE;
-		this.min_maxHeap = Number.MAX_VALUE;
-		var pre = this.maxHeap.pop();
-		this.saveInMaxHeap(num);
-		this.saveInMinHeap(pre.val);
+		bs(num);
 	}
-
-};
-
-MedianFinder.prototype.saveInMinHeap = function(num) {
-	this.min_minHeap = this.minHeap.length !== 0 ? Math.min(num, this.minHeap[this.minHeap.length - 1].min) : num;
-	this.max_minHeap = this.minHeap.length !== 0 ? Math.max(num, this.minHeap[this.minHeap.length - 1].max) : num;
-	this.minHeap.push({
-		val: num,
-		min: this.min_minHeap,
-		max: this.max_minHeap,
-	});
-};
-
-MedianFinder.prototype.saveInMaxHeap = function(num) {
-	this.min_maxHeap = this.maxHeap.length !== 0 ? Math.min(num, this.maxHeap[this.maxHeap.length - 1].min) : num;
-	this.max_maxHeap = this.maxHeap.length !== 0 ? Math.max(num, this.maxHeap[this.maxHeap.length - 1].max) : num;
-	this.maxHeap.push({
-		val: num,
-		min: this.min_maxHeap,
-		max: this.max_maxHeap,
-	});
 };
 
 /**
  * @return {number}
  */
 MedianFinder.prototype.findMedian = function() {
-	var num1 = this.maxHeap[this.maxHeap.length - 1].min;
-	var num2 = this.minHeap[this.minHeap.length - 1].max;
-
-	console.log("result", this.maxHeap.length);
-	console.log("result", this.minHeap.length);
-	return this.maxHeap.length === this.maxHeap.length ?
-		Math.round((num1 + num2) / 2 * 10) / 10 :
-		this.maxHeap.length > this.maxHeap.length ? num1 : num2;
+	const mid = Math.floor(this.arr.length / 2);
+	return (this.arr.length % 2 === 0) ? (this.arr[mid - 1] + this.arr[mid]) / 2 : this.arr[mid];
 };
 
 /**
