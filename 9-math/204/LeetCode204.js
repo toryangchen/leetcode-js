447c8cef7948ddcda0f5ea07ec07e29c17507585
Toryang <toryangchen@outlook.com>
2019-11-19 17:53:38 +0800

    update leetcode 204

---
+++ 9-math/204/LeetCode204.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+	let hash = new Array(n).fill(true)
+	hash[0] = false;
+	hash[1] = false;
+
+	for (let i = 2; i * i < n; i++) {
+		if (hash[i]) {
+			for (let j = i * i; j < n; j += i) {
+				hash[j] = false
+			}
+		}
+	}
+
+	return hash.filter(item => item).length;
+};
