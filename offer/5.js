4560c7854669008cbb9b17edeab68d506d53ba89
Toryang <toryangchen@outlook.com>
2020-01-05 22:24:30 +0800

    update

---
+++ offer/5.js
@@ -0,0 +1,34 @@
+// 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+// 输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+// 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+// NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+
+function minNumberInRotateArray(rotateArray) {
+  // write code here
+  let low = 0,
+    high = rotateArray.length - 1;
+  while (low < high) {
+    let mid = low + (high - low) / 2;
+    if (rotateArray[mid] > rotateArray[high]) {
+      low = mid + 1;
+    } else if (rotateArray[mid] == rotateArray[high]) {
+      high = high - 1;
+    } else {
+      high = mid;
+    }
+  }
+  return rotateArray[low];
+}
+
+function minNumberInRotateArray_1(rotateArray) {
+  // write code here
+  if (rotateArray.length == 0) {
+    return 0;
+  }
+  for (var i = 0, length = rotateArray.length - 1; i < length; i++) {
+    if (rotateArray[i] > rotateArray[i + 1]) {
+      return rotateArray[i + 1];
+    }
+  }
+  return rotateArray[0];
+}
