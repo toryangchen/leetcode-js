5548b35986f337065c96ed53dbb9ae1758d3fdf7
Toryang <toryangchen@outlook.com>
2020-01-06 20:50:23 +0800

    update 剑指offer7

---
+++ offer/7.js
@@ -0,0 +1,22 @@
+//大家都知道斐波那契数列，现在要求输入一个整数n，
+//请你输出斐波那契数列的第n项（从0开始，第0项为0）。n <= 39
+
+function Fibonacci(n) {
+  let result = [];
+  result[0] = 0;
+  result[1] = 1;
+  for (var i = 2; i <= n; i++) {
+    result[i] = result[i - 1] + result[i - 2];
+  }
+  return result[n];
+}
+
+function Fibonacci_1(n) {
+  let result = [];
+  result[0] = 0;
+  result[1] = 1;
+  for (var i = 2; i <= n; i++) {
+    result[i] = result[i - 1] + result[i - 2];
+  }
+  return result[n];
+}
