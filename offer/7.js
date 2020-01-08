e1153aeaca8b9c6ec4c6753c0aae79d5506cd52e
Toryang <toryangchen@outlook.com>
2020-01-08 19:23:42 +0800

    update 剑指offer 7

--- offer/7.js
+++ offer/7.js
@@ -12,11 +12,11 @@ function Fibonacci(n) {
 }
 
 function Fibonacci_1(n) {
-  let result = [];
-  result[0] = 0;
-  result[1] = 1;
-  for (var i = 2; i <= n; i++) {
-    result[i] = result[i - 1] + result[i - 2];
+  if (n == 0) {
+    return 0;
   }
-  return result[n];
+  if (n == 1) {
+    return 1;
+  }
+  return Fibonacci_1(n - 1) + Fibonacci_1(n - 2);
 }
