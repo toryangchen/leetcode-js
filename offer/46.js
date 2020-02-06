bf08c531cc67447061cc62f165621cf195b4b52c
Toryang <toryangchen@outlook.com>
2020-02-06 14:26:06 +0800

    update offer 46

---
+++ offer/46.js
@@ -0,0 +1,10 @@
+// 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及
+// 条件判断语句（A?B:C）。
+
+function Sum_Solution(n) {
+  // write code here
+  if (n == 1) {
+    return n;
+  }
+  return n + Sum_Solution(n - 1);
+}
