9a906105495ca0850c936d1cdb3e8bc382941720
Toryang <toryangchen@outlook.com>
2020-02-06 14:31:42 +0800

    update offer 47

---
+++ offer/47.js
@@ -0,0 +1,11 @@
+// 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+
+function Add(num1, num2) {
+  // write code here
+  while (num2 != 0) {
+    let temp = num1 ^ num2;
+    num2 = (num1 & num2) << 1;
+    num1 = temp;
+  }
+  return num1;
+}
